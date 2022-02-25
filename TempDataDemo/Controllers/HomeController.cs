c5f24490bd9888ede7d7abdf8d76e6b45e4d85a1
Amit <amitkr24031995@gmail.com>
2022-02-25 07:55:58 +0530

    Adding viewTemp

---
+++ TempDataDemo/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TempDataDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewData["Var1"] = "Message from View Data";
+            ViewBag.Var2 = "Message from View Bag";
+            TempData["Var3"] = "Message from Temp Data";
+            string[] games = { "Cricket", "Baseball", "Football", "Hockey" };
+            TempData["GamesArray"] = games;
+            //return RedirectToAction("About");
+            return RedirectToAction("Contact");
+            //return View();
+        }
+        public ActionResult About()
+        {
+            if(TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+            TempData.Keep();
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            if (TempData["Var3"] != null)
+            {
+                TempData["Var3"].ToString();
+            }
+            return View();
+        }
+    }
+}
